908c88a08eca18c79b99d6729e776bc9d7ef9e1b
Alfonso David Recio Calderon <alfonsodavidreciocalderon.guadalupe@alumnado.fundacionloyola.es>
2023-11-22 18:50:59 +0100

    Quinto commit con algunos mockup de usuario, reorganizacion de la distribucion de las carpetas en mockup y style separado en styleAdmin y styleUsuario

--- mockups/admin/basura/gestionbasura.php
+++ mockups/admin/basura/gestionbasura.php
@@ -28,13 +28,13 @@ if (!isset($_GET['mensaje']) || empty($_GET['mensaje'])) {
 <html lang="es">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../../css/styleAdmin.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Gestión de basura</title>
     </head>
     <body>
         <header>
-            <img src="../img/logo.png" alt="Logo de la aplicación">
+            <img src="../../img/logo.png" alt="Logo de la aplicación">
         </header>
         <main id="gestor">
             <h1>Basura</h1>
--- mockups/admin/basura/modificar.php
+++ mockups/admin/basura/modificar.php
@@ -16,13 +16,13 @@
 <html lang="es">
     <head>
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="../css/style.css">
+        <link rel="stylesheet" href="../../css/styleAdmin.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Modificar basura</title>
     </head>
     <body>
         <header>
-            <img src="../img/logo.png" alt="Logo de la aplicación">
+            <img src="../../img/logo.png" alt="Logo de la aplicación">
         </header>
         <main id="gestorM">
             <a href="gestionbasura.php">Volver</a>
