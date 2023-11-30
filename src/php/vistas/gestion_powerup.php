94608504096af7b11d28817cc3582c33632d88f7
Sergio Mosquera Marin <smosqueramarin@gmail.com>
2023-11-30 10:08:22 +0100

    modificada tabla powerup y arreglado su estilo

--- src/php/vistas/gestion_powerup.php
+++ src/php/vistas/gestion_powerup.php
@@ -14,13 +14,13 @@
 ?>
 <main id="gestor">
     <h1>PowerUps</h1>
-    <table>
+    <table id="tablaPowerup">
         <tr>
+            <th></th>
             <th>Nombre</th>
             <th>Aumento</th>
             <th>Imagen</th>
             <th>Descripcion</th>
-            <th>Opción</th>
         </tr>
         <?php
         $obj = new $controlador();
@@ -30,13 +30,13 @@
         foreach ($datos as $fila) {
             ?>
             <tr>
+                <td>
+                    <a href="index.php?control=powerup_con&metodo=buscarModificar&id=<?php echo $fila['id']; ?>">✏️</a>
+                </td>
                 <td><?php echo $fila['nombre']; ?></td>
                 <td><?php echo $fila['aumento']; ?></td>
                 <td><img src="data:image/png;base64,<?php echo $fila['imagen'];?>"></td>
                 <td><?php echo $fila['descripcion']; ?></td>
-                <td>
-                    <a href="index.php?control=powerup_con&metodo=buscarModificar&id=<?php echo $fila['id']; ?>">✏️</a>
-                </td>
             </tr>
             <?php
         }
