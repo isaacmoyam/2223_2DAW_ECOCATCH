f4ca84a1cda2fc9a20b8ea150da1940d100640d2
Your Name <you@example.com>
2023-12-01 09:50:00 +0100

    cambiar nombre niveles administrador

--- src/php/controladores/nivel_con.php
+++ src/php/controladores/nivel_con.php
@@ -40,7 +40,7 @@ class Nivel_con {
      */
     public function __construct() {
         $this->pagina = "Gestión de niveles";        
-        $this->vista = 'gestionnivel';
+        $this->vista = 'gestion_nivel';
         $this->obj = new Nivel_Mod();
     }
 
@@ -96,7 +96,7 @@ class Nivel_con {
      */
     public function vistaCrear() { 
         $this->pagina = "Crear nivel"; 
-        $this->vista = 'anadirNivel';
+        $this->vista = 'anadir_nivel';
     }
 
     /**
@@ -105,7 +105,7 @@ class Nivel_con {
      */
     public function buscarModificar() { 
         $this->pagina = "Modificar nivel"; 
-        $this->vista = 'modificarNivel';
+        $this->vista = 'modificar_nivel';
         return $this->obj->buscarModificar($_GET["id"]);
     }
 
@@ -184,7 +184,7 @@ class Nivel_con {
      */
     public function mostrarMensajes() {
         $this->pagina = "Modificar nivel"; 
-        $this->vista = 'modificarNivel';
+        $this->vista = 'modificar_nivel';
         return $this->obj->mostrarMensajes($_GET["id"]);
     }
 
@@ -192,7 +192,7 @@ class Nivel_con {
      * Borra un mensaje asociado a un nivel.
      */
     public function borrarMensaje() {
-        $this->vista = 'gestionnivel';
+        $this->vista = 'gestion_nivel';
         $this->obj->borrarMensaje($_GET["id"]);
         header("Location: index.php?control=nivel_con&metodo=buscarModificar&id=".$_GET['idNivel']);
     }
@@ -203,7 +203,7 @@ class Nivel_con {
      */
     public function buscarMensaje() {
         $this->pagina = "Modificar mensaje"; 
-        $this->vista = 'modificarMensaje';
+        $this->vista = 'modificar_mensaje';
         return $this->obj->buscarMensaje($_GET["id"]);
     }
 
