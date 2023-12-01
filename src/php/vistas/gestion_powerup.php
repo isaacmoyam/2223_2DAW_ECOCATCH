6e17a08ccf573c34ea1e465e803a8b794f7d994f
Your Name <you@example.com>
2023-12-01 10:14:33 +0100

    powerup

---
+++ src/php/vistas/gestion_powerup.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Página principal para la gestión de powerup.
+ *
+ * PHP version 8.2
+ *
+ * @category PowerUp
+ * @package  Gestión_Basura
+ * @author   Equipo A
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ */
+
+// Sección HTML para la interfaz de gestión de powerup.
+?>
+<main id="gestor">
+    <h1>PowerUps</h1>
+    <table>
+        <tr>
+            <th>Opción</th>
+            <th>Nombre</th>
+            <th>Aumento</th>
+            <th>Imagen</th>
+            <th>Descripcion</th>
+        </tr>
+        <?php
+        $obj = new $controlador();
+        $datos = $obj->mostrar();
+
+        // Iteración sobre los datos para mostrar en la tabla.
+        foreach ($datos as $fila) {
+            ?>
+            <tr>
+                <td>
+                    <a href="index.php?control=powerup_con&metodo=buscarModificar&id=<?php echo $fila['id']; ?>">✏️</a>
+                </td>
+                <td><?php echo $fila['nombre']; ?></td>
+                <td><?php echo $fila['aumento']; ?></td>
+                <td><img src="data:image/png;base64,<?php echo $fila['imagen'];?>"></td>
+                <td><?php echo $fila['descripcion']; ?></td>
+            </tr>
+            <?php
+        }
+        ?>
+    </table>
+    <a href="index.php?control=powerup_con&metodo=restarurarValoresPowerup">Restaurar valores por defecto</a>
+    <?php echo "<p>".$mensaje."</p>" ?>
+</main>
---
+++ src/php/vistas/modificar_powerup.php
@@ -0,0 +1,22 @@
+<main id="gestor">
+    <h1>Modificación Powerup</h1>
+    <?php
+    $fila = $datosVista["datos"];
+    ?>
+    <!--enctype="multipart/form-data" es para que se pueda pasar el FILE -->
+    <form enctype="multipart/form-data" action="index.php?control=powerup_con&metodo=modificar&id=<?php echo $fila['id'];?>" method="POST" id="formPowerup">
+        <label>(*) Nombre:</label>
+        <input type="text" name="nombre" value="<?php echo $fila['nombre'];?>" placeholder="Nombre Powerup">
+        <label>(*) Aumento:</label>
+        <input type="text" name="aumento" value="<?php echo $fila['aumento'];?>" placeholder="Aumento">
+        <label>(*) Imagen:</label>
+        <input type="file" name="imagen">
+        <label>Descripción:</label>
+        <input type="text" name="descripcion" value="<?php echo $fila['descripcion'];?>">
+        <label>Imagen Actual:</label>
+        <img src="data:image/png;base64,<?php echo $fila['imagen'];?>" alt="Imagen Actual">
+        <img id="imagenMiniatura">
+        <p id="msgCampos"></p>
+        <input type="submit" value="Modificar Powerup">
+    </form>
+</main>
