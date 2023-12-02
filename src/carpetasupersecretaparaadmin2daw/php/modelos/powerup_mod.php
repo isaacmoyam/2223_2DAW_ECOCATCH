301fae2fa791a85020bcf9e9ffddda4b73f8b543
Alfonso David Recio Calderon <alfonsodavidreciocalderon.guadalupe@alumnado.fundacionloyola.es>
2023-12-02 12:00:51 +0100

    Vigesimo octavo commit con cambios en powerup como la manera en la que se establece y cierra conexion

--- src/carpetasupersecretaparaadmin2daw/php/modelos/powerup_mod.php
+++ src/carpetasupersecretaparaadmin2daw/php/modelos/powerup_mod.php
@@ -14,35 +14,17 @@ class Powerup_Mod
 
     /**
      * Instancia de la conexión a la base de datos.
-     * @var mysqli
+     * @var conexion
      */
-    private $mysqli;
+    private $conexion;
 
     /**
      * Constructor de la clase Powerup_Mod.
      */
     public function __construct()
     {
-
-    }
-
-    /**
-     * Establece la conexión con la base de datos.
-     */
-    public function establecerConexion()
-    {
         $dbObj = new Db();
-        $this->mysqli = $dbObj->mysqli;
-    }
-
-    /**
-     * Cierra la conexión con la base de datos.
-     */
-    public function cerrarConexion()
-    {
-        if ($this->mysqli) {
-            mysqli_close($this->mysqli);
-        }
+        $this->conexion = $dbObj->mysqli;
     }
 
 
@@ -51,17 +33,16 @@ class Powerup_Mod
      * @return array
      */
     public function mostrar() {
-        $this->establecerConexion();
         $sql = "SELECT p.id, i.nombre, i.imagen, p.aumento, p.descripcion FROM powerup p INNER JOIN item i ON p.id = i.id";
 
-        $result = $this->mysqli->query($sql);
+        $result = $this->conexion->query($sql);
 
         $powerups = array();
         while ($row = $result->fetch_assoc()) {
             $powerups[] = $row;
         }
 
-        $this->cerrarConexion();
+        $this->conexion->close(); //Cerrar conexion
 
         return $powerups;
     }
@@ -72,11 +53,10 @@ class Powerup_Mod
      * @return array
      */
     public function buscarModificar($id) {
-        $this->establecerConexion();
         $sql = 'SELECT p.id, i.nombre, i.imagen, p.aumento, p.descripcion FROM powerup p INNER JOIN item i ON p.id=i.id WHERE i.id='.$id;
-        $result = $this->mysqli->query($sql);
+        $result = $this->conexion->query($sql);
 
-        $this->cerrarConexion();
+        $this->conexion->close(); //Cerrar conexion
 
         $fila = $result->fetch_assoc();
 
@@ -92,19 +72,13 @@ class Powerup_Mod
      * @return bool
      */
     public function modificar($id, $nombre, $imagen, $aumento, $descripcion) {
-        $this->establecerConexion();
-
         try {
             $sql = 'UPDATE item SET nombre = "'.$nombre.'", imagen = "'.$imagen.'" WHERE id = '.$id;
-            $result = $this->mysqli->query($sql);
+            $result = $this->conexion->query($sql);
         } catch(mysqli_sql_exception $e) {
             $error = true;
             return $error;
         }
-
-        $this->cerrarConexion();
-
-        $this->establecerConexion();
 
         try {
             /*
@@ -114,13 +88,13 @@ class Powerup_Mod
              * si la condicion es verdadera devuelve null y en casso contrario devuelve '"'.$descripcion.'"'
              * */
             $sql = 'UPDATE powerup SET aumento = '.$aumento.' ,descripcion = '.($descripcion === "" ? 'NULL' : '"'.$descripcion.'"').'  WHERE id = '.$id;
-            $result = $this->mysqli->query($sql);
+            $result = $this->conexion->query($sql);
         } catch(mysqli_sql_exception $e) {
             $error = true;
             return $error; //Si hay un error devulve true
         }
 
-        $this->cerrarConexion();
+        $this->conexion->close(); //Cerrar conexion
     }
 
     /**
@@ -129,25 +103,24 @@ class Powerup_Mod
      * @return void
      */
     public function valoresPorDefecto($imagen) {
-        $this->establecerConexion();
         /*
          * En el contexto del WHERE 1, el 1 es una condición siempre verdadera lo que significa que todas las filas que cumplan la condicion seran eliminadas
          * Por ello como no quiero borrar todas las filas de item y solo quiero borrar las filas de item que esten en power up
          * hago el INNER JOIN powerup p ON i.id=p.id para que así solo me borren las filas de item que estan en powerup
          * */
         $sqlEliminarPowerup = 'DELETE i FROM item i INNER JOIN powerup p ON i.id=p.id WHERE 1';
-        $this->mysqli->query($sqlEliminarPowerup);
+        $this->conexion->query($sqlEliminarPowerup);
 
         //Consulta Preparada para item
         $sqlItem = 'INSERT INTO item (nombre, imagen) VALUES (?, ?)';
 
-        $consultaPrepardaItem = $this->mysqli->prepare($sqlItem);
+        $consultaPrepardaItem = $this->conexion->prepare($sqlItem);
         $consultaPrepardaItem->bind_param('ss', $nombrePorDefecto, $imagen);
 
         //Consulta Preparada para powerup
         $sqlPowerup = 'INSERT INTO powerup (id, aumento, descripcion) VALUES (?, ?, ?)';
 
-        $consultaPreparadaPowerup = $this->mysqli->prepare($sqlPowerup);
+        $consultaPreparadaPowerup = $this->conexion->prepare($sqlPowerup);
         $consultaPreparadaPowerup->bind_param('iis', $idPorDefecto, $aumentoPorDefecto, $descripcionPorDefecto);
 
         //Preparo el array con los datosque quiero introducir
@@ -164,7 +137,7 @@ class Powerup_Mod
             $consultaPrepardaItem->execute(); //Ejecutar consulta preparada item
 
             //Dar valor a las variables para la consulta de powerup
-            $idPorDefecto = $this->mysqli->insert_id; //Obtengo el id de la ultima consulta (insert de item)
+            $idPorDefecto = $this->conexion->insert_id; //Obtengo el id de la ultima consulta (insert de item)
             $aumentoPorDefecto = $dato[1];
             $descripcionPorDefecto = $dato[2];
 
@@ -175,28 +148,26 @@ class Powerup_Mod
         $consultaPrepardaItem->close();
         $consultaPreparadaPowerup->close();
 
-        $this->cerrarConexion();
+        $this->conexion->close(); //Cerrar conexion
     }
 
     /**
      * Recoge datos de power-ups y los retorna al controlador en json.
      */
     public function ajaxDatosPowerup() {
-        $this->establecerConexion();
-
         /*
          * Consulta para obtener información de power-ups
          * Descripcion no se manda dado que no se necesitara para nada
          * */
         $sqlPowerup = "SELECT p.id, i.nombre, i.imagen, p.aumento FROM powerup p INNER JOIN item i on p.id = i.id";
-        $resultPowerup = $this->mysqli->query($sqlPowerup);
+        $resultPowerup = $this->conexion->query($sqlPowerup);
 
         $powerups = array();
         while ($row = $resultPowerup->fetch_assoc()) {
             $powerups[] = $row;
         }
 
-        $this->cerrarConexion();
+        $this->conexion->close(); //Cerrar conexion
 
         return json_encode($powerups);
     }
